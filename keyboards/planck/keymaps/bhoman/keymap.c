92501c64b2dbe195a5023a174f3e7dca15698e52
Benjamin Homan <ben@benhoman.com>
2019-04-26 21:12:54 -0400

    Updated planck

--- keyboards/planck/keymaps/bhoman/keymap.c
+++ keyboards/planck/keymaps/bhoman/keymap.c
@@ -23,6 +23,7 @@ enum planck_layers {
   _RAISE,
   _LOWER,
   _NAV,
+  _WM,
 };
 
 enum planck_keycodes {
@@ -31,6 +32,29 @@ enum planck_keycodes {
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+
+// i3 Window Manager
+/*#define WS1 LGUI(KC_1)
+#define WS2 LGUI(KC_2)
+#define WS3 LGUI(KC_3)
+#define WS4 LGUI(KC_4)
+#define WS5 LGUI(KC_5)
+#define WS6 LGUI(KC_6)
+#define WS7 LGUI(KC_7)
+#define WS8 LGUI(KC_8)
+#define WS9 LGUI(KC_9)
+#define WS10 LGUI(KC_0)
+#define MWS1 SGUI(KC_1)
+#define MWS2 SGUI(KC_2)
+#define MWS3 SGUI(KC_3)
+#define MWS4 SGUI(KC_4)
+#define MWS5 SGUI(KC_5)
+#define MWS6 SGUI(KC_6)
+#define MWS7 SGUI(KC_7)
+#define MWS8 SGUI(KC_8)
+#define MWS9 SGUI(KC_9)
+#define MWS10 SGUI(KC_0)
+*/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -82,10 +106,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F0,   KC_BSLS,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_BSLS,
     KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LGUI,
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BRID, KC_BRIU, KC_RSFT,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX
+),
+
+/* WM 
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      | Home | PgUp | PgDn |  End | Left | Down |  Up  |Right |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_WM] = LAYOUT_planck_grid(
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX 
 ),
 
 /* Nav
